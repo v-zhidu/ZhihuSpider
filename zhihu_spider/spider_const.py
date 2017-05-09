4e2663e0deed2bb2897697769ec6fd417ba9127c
Zhiqiang Du <du_zhi_qiang@163.com>
2017-05-09 09:38:03 +0800

    修改爬取范围

--- zhihu_spider/spider_const.py
+++ zhihu_spider/spider_const.py
@@ -9,4 +9,4 @@ spider_const.py create by v-zhidu
 ZHIHU_HOST = 'https://www.zhihu.com'
 ZHIHU_TOPICS = ZHIHU_HOST + '/topics'
 ZHIHU_TOPIC_LIST_API = ZHIHU_HOST + '/node/TopicsPlazzaListV2'
-TOPIC_OFF_SET_MAX = 20
+TOPIC_OFF_SET_MAX = 2000
